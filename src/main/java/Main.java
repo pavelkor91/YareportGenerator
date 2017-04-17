12c5d298f1cff825d3805ef7182e5f12bded7bc0
pavelkor91 <pavelkor91@gmail.com>
2017-04-18 01:24:12 +0300

    Created token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import com.sun.jndi.toolkit.url.Uri;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Main {
 
-    public static void main (String [] args) throws URISyntaxException{
+    public static void main (String [] args) throws URISyntaxException, IOException{
         Token token = new Token("0b3f39cc14d240ed8386d139b457c263","4c47e2453bfc4c9e8e5453d9248b9aa9");
         token.requestToken();
     }
--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -1,14 +1,21 @@
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 import javax.imageio.IIOException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Token {
@@ -20,8 +27,8 @@ public class Token {
         this.client_id = client_id;
         this.client_secret = client_secret;
     }
-    public void requestToken() throws URISyntaxException{
-        String url = "https://oauth.yandex.ru/authorize?response_type=token&client_id="+client_id;
+    public void requestToken() throws URISyntaxException, IOException{
+        String url = "https://oauth.yandex.ru/authorize?response_type=code&client_id="+client_id;
         URI uri;
         try{
             uri = new URI(url);
@@ -36,6 +43,25 @@ public class Token {
 
         Scanner in = new Scanner(System.in);
         token = in.next();
+        String tokenUrl = "https://oauth.yandex.ru/token";
+        uri = new URI(tokenUrl);
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpUriRequest request = RequestBuilder.post(uri)
+                .addHeader("Content-type:", "application/x-www-form-urlencoded")
+                .addParameter("grant_type", "authorization_code")
+                .addParameter("code", token)
+                .addParameter("redirect_uri", "seolt.ru")
+                .addParameter("client_id", "client_id")
+                .addParameter("client_secret", client_secret)
+                .setEntity(new UrlEncodedFormEntity(Arrays.asList(
+                        new BasicNameValuePair("end2br_desc", "1"),
+                        new BasicNameValuePair("includehtml_desc", "no")
+                )))
+                .build();
+        System.out.println(request.toString());
+        HttpResponse response = httpClient.execute(request);
+        System.out.println(response.toString());
+
 //        String result;
 //        uri = new URI(url);
 //        HttpGet req = new HttpGet(uri);
